568e8cb6fd2969cb4a316c3bc85edd52f515a2a1
Tobias Kloht <tobias.kloht@klarna.com>
2020-08-14 23:05:35 +0200

    CI is case-sensitive, i shall never learn

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,4 +1,4 @@
-import './matchMedia.mock'; // Must be imported before the tested file
+import './matchmedia.mock';
 import * as React from 'react';
 import { render } from '@testing-library/react';
 import { useMediamux, MediamuxProvider } from '../src';
