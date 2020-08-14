657ed93eff107351cd5c3a044a8f3519ca50c08e
Tobias Kloht <tobias.kloht@klarna.com>
2020-08-14 15:42:42 +0200

    working prototype

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,24 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { useMediamux, MediamuxProvider } from '../.';
 
 const App = () => {
   return (
-    <div>
-      <Thing />
-    </div>
+    <MediamuxProvider theme={{breakpoints: ["768px", "1200px"]}}>
+      <Example />
+    </MediamuxProvider>
   );
 };
+
+function Example() {
+  const mmx = useMediamux()
+  return (
+    <div>
+      <div>hello there</div>
+      {mmx(["smol", "medium", "large"])}
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,65 @@
 import * as React from 'react';
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
+type MediamuxContextType = {
+  matchingQueries: boolean[];
 };
+
+const initialValues: MediamuxContextType = {
+  matchingQueries: [],
+};
+
+const MediamuxContext = React.createContext(initialValues);
+
+type Theme = {
+  breakpoints: String[];
+};
+
+type Props = {
+  theme: Theme;
+  children: React.ReactNode;
+};
+
+function MediamuxProvider({ theme, children }: Props) {
+  const { breakpoints } = theme;
+  const mediaQueries = breakpoints.map(n => `(min-width: ${n})`);
+
+  const [matchingQueries, setMatchingQueries] = React.useState(
+    mediaQueries.map(x => window.matchMedia(x).matches)
+  );
+
+  // todo: fix "any"
+  const handleQueryChange = (
+    index: number,
+    currentMatchingQueries: boolean[]
+  ) => (x: any) => {
+    console.log('handle query change: ', index, x, currentMatchingQueries);
+    const newMatchingQueries = [...currentMatchingQueries];
+
+    newMatchingQueries[index] = x.matches;
+    setMatchingQueries(newMatchingQueries);
+  };
+
+  React.useEffect(() => {
+    const matchMediaQueries = mediaQueries.map(x => window.matchMedia(x));
+    for (let index = 0; index < matchMediaQueries.length; index++) {
+      const mq = matchMediaQueries[index];
+      mq.addEventListener('change', handleQueryChange(index, matchingQueries));
+    }
+  }, [mediaQueries]);
+
+  return (
+    <MediamuxContext.Provider value={{ matchingQueries }}>
+      {children}
+    </MediamuxContext.Provider>
+  );
+}
+
+function useMediamux() {
+  const { matchingQueries } = React.useContext(MediamuxContext);
+  const matchingQueriesCount = matchingQueries.filter((x: boolean) => x).length;
+
+  console.log('>>> ', { matchingQueriesCount });
+  return (x: unknown[]) => x[matchingQueriesCount];
+}
+
+export { useMediamux, MediamuxProvider, MediamuxContext as __Context };
