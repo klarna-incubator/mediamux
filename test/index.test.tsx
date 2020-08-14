6b98c8d7bfba01e9d59b96f1cc38041bbdae71ee
Tobias Kloht <tobias.kloht@klarna.com>
2020-08-14 18:14:10 +0200

    add more tests

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -26,4 +26,98 @@ describe('it', () => {
 
     expect(queryByTestId('small')).toBeTruthy();
   });
+
+  it('renders first argument if matching first media-query', () => {
+    (window.matchMedia as any).mockImplementationOnce((query: any) => ({
+      matches: true,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    }));
+
+    const { queryByTestId } = render(
+      <MediamuxProvider>
+        <TestComponent />
+      </MediamuxProvider>
+    );
+
+    expect(queryByTestId('medium')).toBeTruthy();
+  });
+
+  it('renders second argument if matching first and second media-query', () => {
+    (window.matchMedia as any)
+      .mockImplementationOnce((query: any) => ({
+        matches: true,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      }))
+      .mockImplementationOnce((query: any) => ({
+        matches: true,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      }));
+
+    const { queryByTestId } = render(
+      <MediamuxProvider>
+        <TestComponent />
+      </MediamuxProvider>
+    );
+
+    expect(queryByTestId('large')).toBeTruthy();
+  });
+
+  it('picks last argument if active mediaquery index > argument array', () => {
+    (window.matchMedia as any)
+      .mockImplementationOnce((query: any) => ({
+        matches: true,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      }))
+      .mockImplementationOnce((query: any) => ({
+        matches: true,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      }));
+
+    function OnlyTwoItems() {
+      const mmx = useMediamux();
+      return (
+        <div>
+          {mmx([<div data-testid="small" />, <div data-testid="medium" />])}
+        </div>
+      );
+    }
+
+    const { queryByTestId } = render(
+      <MediamuxProvider>
+        <OnlyTwoItems />
+      </MediamuxProvider>
+    );
+
+    expect(queryByTestId('large')).toBeTruthy();
+  });
 });
